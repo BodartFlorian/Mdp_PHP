9763e6bd97b40a3031ad64c149c893e656d1de3f
FlorianBodart <bodart.florian@outlook.fr>
2022-05-06 15:10:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php session_start();
+if(isset($_SESSION['table'])) $table = $_SESSION['table'];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <?php  include 'includes/head.inc.html';  ?>
+<body>
+   
+
+
+ 
+
+ 
+
+</body>
+</html>
