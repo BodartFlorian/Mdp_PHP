9d96651a443bd60e3cbaf695c939debc0c877a1d
FlorianBodart <bodart.florian@outlook.fr>
2022-05-10 13:24:32 +0200

    version 0.0.6 add modal

--- index.php
+++ index.php
@@ -25,30 +25,10 @@
                 Tester un mot de passe
             </button>
         </div>
-        <div class="modal" tabindex="1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                    </div>
-                    <div class="modal-body">
-
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                        <button type="button" class="btn btn-primary">Tester</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
                     <div class="modal-body">
                         <div class="form-floating">
                             <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
@@ -56,8 +36,8 @@
                         </div>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                        <button type="button" class="btn btn-primary" type="submit">Tester</button>
                     </div>
                 </div>
             </div>
