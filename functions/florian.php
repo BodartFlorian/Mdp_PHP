5136a6ee9edf827209d3a83cead12dd0fc6c5301
FlorianBodart <bodart.florian@outlook.fr>
2022-05-10 11:43:15 +0200

    version 0.0.4 add visual

--- functions/florian.php
+++ functions/florian.php
@@ -8,59 +8,94 @@ function checkPassword($password)
     $valueRod = 0;
 
     echo '<h1 class="text-center">Florian</h1>
-    <p class="w-100">Mot de passe : '.$password.'</p>
+    <p class="w-100">Mot de passe : ' . $password . '</p>
     <p class="text-end mb-0">Force du mot de passe</p>';
 
     // Validation de la force du mot de passe 
-    $uppercase =  preg_match('@[AZ]@',  $password);
-    $lowercase = preg_match('@[az]@', $password);
     $number = preg_match('@[0-9]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $uppercase =  preg_match('@[A-Z]@',  $password);
     $specialChars = preg_match('@[^\w]@', $password);
-    
 
-        if (!$uppercase) {
-            echo 'manque MAJ';
-            
-        } else {
-            $valueRod += 20;
-            echo 'oui maj';
-        }
+    // check strenght password & increment
+    if (!$number) {
+        $displayNumber = '';
+    } else {
+        $valueRod += 20;
+        $displayNumber = 'd-none';
+    }
 
-        if (!$lowercase) {
-            echo 'manque min';
-        } else {
-            $valueRod += 20;
-            echo 'oui min';
-        }
+    if (!$lowercase) {
+        $displayLower = '';
+    } else {
+        $valueRod += 20;
+        $displayLower = 'd-none';
+    }
 
-        if (!$number) {
-            echo 'manque nbr';
-        } else {
-            $valueRod += 20;
-            echo 'oui nbr';
-        }
+    if (!$uppercase) {
+        $displayUpper = '';
+    } else {
+        $valueRod += 20;
+        $displayUpper = 'd-none';
+    }
 
-        if (!$specialChars) {
-            echo 'manque spéchar';
-        } else {
-            $valueRod += 20;
-            echo 'oui spéchar';
-        }
+    if (!$specialChars) {
+        $displaySpecialChars = '';
+    } else {
+        $valueRod += 20;
+        $displaySpecialChars = 'd-none';
+    }
 
-
-        if (strlen($password) < 12) {
-            echo ' au moins 12 caractères';
-        } else {
-            $valueRod += 20;
-            echo 'oui 12 carac';
-        }
+    if (strlen($password) < 12) {
+        $displayLenght = '';
+    } else {
+        $valueRod += 20;
+        $displayLenght = 'd-none';
+    }
 
 
+    // switch color rod with increment
+    switch ($valueRod) {
+        case 20:
+            $colorRod = 'bg-danger';
+            break;
+        case 40:
+            $colorRod = 'bg-warning';
+            break;
+        case 60:
+            $colorRod = 'bg-info';
+            break;
+        case 80:
+            $colorRod = '';
+            break;
+        case 100:
+            $colorRod = 'bg-success';
+            $displayContainer = 'd-none';
+            $buttonOk = '<a href="http://localhost/index.php" type="button" class="btn btn-success">Success</a>';
+            break;
+    }
+
+
+    // strenght rod 
     echo '<div class="progress">
-     <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0"      aria-valuemax="100" style="width: ' . $valueRod . '%"></div>
+     <div class="progress-bar progress-bar-striped progress-bar-animated ' . $colorRod . '" role="progressbar" aria-valuenow="75" aria-valuemin="0"      aria-valuemax="100" style="width: ' . $valueRod . '%"></div>
     </div>';
+
+
+    // missing something about password
+    echo '<div class="container ' . $displayContainer . '">
+            <div class="row align-middle">
+                <ul class="list-group col-4">
+                    <li class="list-group-item active" aria-current="true">Le mot de passe doit contenir au moins:</li>
+                    <li class="list-group-item ' . $displayNumber . '">1 chiffre</li>
+                    <li class="list-group-item ' . $displayLower . '">1 minuscule</li>
+                    <li class="list-group-item ' . $displayUpper . '">1 majuscule</li>
+                    <li class="list-group-item ' . $displaySpecialChars . '">1 caratcère spécial</li>
+                    <li class="list-group-item ' . $displayLenght . '">12 caractères</li>
+                </ul>
+            </div>
+        </div>';
+    echo $buttonOk;
 }
 
 checkPassword($password);
-
-
--- functions/test.php
+++ functions/test.php
@@ -1,32 +1,24 @@
 <?php 
 
 
-   // echo '<button type="button" class="btn btn-outline-secondary">Tester un mot de passe</button>';
-    // echo '<div class="modal" tabindex="-1">
-    //         <div class="modal-dialog">
-    //             <div class="modal-content">
-    //             <div class="modal-header">
-    //             </div>
-    //                 <div class="modal-body">
-    //                 <div class="form-floating">
-    //     <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-    //     <label for="floatingPassword">Password</label>
-    // </div>
-    //             </div>
-    //             <div class="modal-footer">
-    //                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-    //                 <button type="button" class="btn btn-primary">Tester</button>
-    //             </div>
-    //             </div>
-    //         </div>
-    //         </div>';
+   echo '<button type="button" class="btn btn-outline-secondary">Tester un mot de passe</button>';
+    echo '<div class="modal" tabindex="-1">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                    <div class="modal-body">
+                    <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                    <button type="button" class="btn btn-primary">Tester</button>
+                </div>
+                </div>
+            </div>
+            </div>';
   
-
-
-
-    //   <div class="progress-bar progress-bar-striped" style="width: 10%" aria-valuenow="10" aria-valuemax="100"></div> 
-    //   <div class="progress-bar progress-bar-striped bg-success"  style="width: 25%" aria-valuenow="25" aria-valuemax="100"></div>
-    //   <div class="progress-bar progress-bar-striped bg-info"  style="width: 50%" aria-valuenow="50"aria-valuemax="100"></div>
-    //   <div class="progress-bar progress-bar-striped bg-warning"  style="width: 75%" aria-valuenow="75"aria-valuemax="100"></div>
-    //   <div class="progress-bar progress-bar-striped bg-danger"  style="width: 100%" aria-valuenow="100" aria-valuemax="100"></div>
-?>
